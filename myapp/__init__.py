61f21b6cb49dc627b64506e43ddc76a37ae31064
Philip Afable <philafable@gmail.com>
2020-12-11 03:00:41 -0500

    changed to app

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '38afdc77e964d6d4bd34258e5185bf1c5a42e414'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/database'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
