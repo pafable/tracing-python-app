3036d6ce1fec021f8ba79b495423e463cd869aa1
Philip Afable <philafable@gmail.com>
2020-12-11 03:17:39 -0500

    created runtime.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ x = os.listdir('myapp')
 if x.__len__() < 10:
     create_db()
 
-# y = User.query.all()
-# if y.__len__() == 0:
-#     create_admin('admin', 'admin', 'admin@example.test')
+y = User.query.all()
+if y.__len__() == 0:
+    create_admin('admin', 'admin', 'admin@example.test')
